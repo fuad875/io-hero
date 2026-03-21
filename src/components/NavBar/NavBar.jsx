9e089509328298dd38271b696329fe9f90a2c0c7
fuad875 <fuad1115279890@gmail.com>
2026-03-21 22:05:05 +0600

    initital setup

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router';
+import lodingImg from '../../assets/photo/logo.png'
+
+const NavBar = () => {
+    const links = <>
+        <Link to="/"><span className='font-bold mx-2 px-2 rounded hover:bg-sky-300'>Home</span></Link>
+        <Link to="/apps"><span className='font-bold mx-2 px-2 rounded hover:bg-sky-300'>Apps</span></Link>
+        <Link to="/installation"><span className='font-bold mx-2 px-2 rounded hover:bg-sky-300'>Installation</span></Link>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                           {links}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">HERO.IO</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Contribute</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
+import App from '../../App';
+import Apps from '../Pages/Apps/Apps';
+import Installation from '../Pages/Installation/Installation';
+
+export const router = createBrowserRouter([
+    {
+       path:"/",
+       Component:Root,
+       errorElement:<ErrorPage></ErrorPage>,
+       children:[
+        {
+            index:true,
+            path:"/",
+            Component:Home,
+        },
+        {
+            path:"/apps",
+            Component:Apps,
+        },
+        {
+            path:"/installation",
+            Component:Installation,
+        },
+       ]
+
+    }
+]);
